176a18135515c4d0518a9319cbf2a2287822cc91
ninipo7 <zxvb488@gmail.com>
2026-09-17 21:15:15 +0330

    Update HFJApplication.kt

--- app/src/main/java/com/hfj/blogreader/HFJApplication.kt
+++ app/src/main/java/com/hfj/blogreader/HFJApplication.kt
@@ -1,14 +1,1 @@
-package com.hfj.blogreader
-
-import android.app.Application
-import com.hfj.blogreader.utils.CrashHandler
-
-class HFJApplication : Application() {
-
-    override fun onCreate() {
-        super.onCreate()
-
-        val crashHandler = CrashHandler(this)
-        Thread.setDefaultUncaughtExceptionHandler(crashHandler)
-    }
-}
+//error n
