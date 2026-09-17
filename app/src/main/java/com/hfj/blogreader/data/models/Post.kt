eda61349e901d62437b9aa3f6aad545cb9b31f78
ninipo7 <zxvb488@gmail.com>
2026-09-17 21:27:14 +0330

    Update Post.kt

--- app/src/main/java/com/hfj/blogreader/data/models/Post.kt
+++ app/src/main/java/com/hfj/blogreader/data/models/Post.kt
@@ -1,12 +1,1 @@
-package com.hfj.blogreader.data.models
-
-data class Post(
-    val id: String,
-    val title: String? = null,
-    val content: String,
-    val imageUrls: List<String> = emptyList(),  // چند عکس
-    val videoUrl: String? = null,
-    val date: String,
-    val hashtags: List<String> = emptyList(),   // هشتگ نداریم
-    val views: String = "0"
-)
+//error n
