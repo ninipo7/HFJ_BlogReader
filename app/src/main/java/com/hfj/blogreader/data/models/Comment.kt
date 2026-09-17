34db1b1f32fb286a2d99cc8d916ec99a7fcc17f6
ninipo7 <zxvb488@gmail.com>
2026-09-17 21:27:46 +0330

    Update Comment.kt

--- app/src/main/java/com/hfj/blogreader/data/models/Comment.kt
+++ app/src/main/java/com/hfj/blogreader/data/models/Comment.kt
@@ -1,11 +1,1 @@
-package com.hfj.blogreader.data.models
-
-data class Comment(
-    val id: String,
-    val postId: String,
-    val userId: String,
-    val userName: String,
-    val text: String,
-    val timestamp: Long,
-    val approvedAt: Long? = null
-)
+//error n
