e4942dc41399045373900c408ec3e475ad01649a
ninipo7 <zxvb488@gmail.com>
2026-09-17 21:10:27 +0330

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,18 +1,1 @@
-pluginManagement {
-    repositories {
-        google()
-        mavenCentral()
-        gradlePluginPortal()
-    }
-}
-dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
-    repositories {
-        google()
-        mavenCentral()
-        maven { url = uri("https://jitpack.io") }   // ✅ این خط را اضافه کنید
-    }
-}
-
-rootProject.name = "HFJ_BlogReader"
-include(":app")
+//error n
